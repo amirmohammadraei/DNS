b51ea9cae26e27fd4c38418cc7e6ba5ee19f2f4f
amiroo <amir.m.raei@gmail.com>
2021-03-29 11:03:06 +0430

    2.1 socket created

---
+++ 2/1/sock.py
@@ -0,0 +1,8 @@
+import socket
+
+
+try: 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print (f"Socket created!")
+except socket.error as err: 
+    print (f"socket creation failed because of error {err}!")
